633007043db958c65beddae38d30fe2e00335412
Syeda Ayesha <syedaayeshawajahat@gmail.com>
2024-08-20 19:52:34 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
