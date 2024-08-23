cc94b1bf6da2e4e55b93646f7820c1489d674672
Syeda Ayesha <syedaayeshawajahat@gmail.com>
2024-08-23 23:23:52 +0500

    Navbar in progress

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,23 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Stars
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <div className='relative z-0 bg-primary'>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div>
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </div>
+    </BrowserRouter>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+import { styles } from '../styles'
+
+import { navLinks } from '../constants'
+import { logo, menu, close } from '../assets'
 
 const Navbar = () => {
+  const [Active, setActive] = useState("");
   return (
-    <div>Navbar</div>
+
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link to='/' className='flex items-center gap-2' onClick={() => {
+          setActive("");
+          scrollTo(0, 0);
+          }}>
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer'> Syeda Ayesha <span className='sm:block hidden'>| Computer Scientist</span></p>
+        </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link)=>(
+            <li id={link.id} className=''>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
   )
 }
 
