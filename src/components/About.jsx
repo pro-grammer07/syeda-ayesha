a6d5a429595cd8655e2696bc1796309a279c3799
Syeda Ayesha <syedaayeshawajahat@gmail.com>
2024-09-02 02:40:15 +0500

    projects

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import SectionWrapper from "../hoc";
 
 const ServiceCard = ({ title, icon, index }) => {
   return (
@@ -22,7 +23,7 @@ const ServiceCard = ({ title, icon, index }) => {
           className="bg-tertiary rounded-[20px] py-5 min-h-[280px] flex justify-evenly items-center flex-col"
         >
           <img src={icon} alt={title} className="w-16 h-16 object-contain"/>
-          <h3 className="text-white text-[20px] font-bold text-center">{titled}</h3>
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
         </div>
       </motion.div>
     </Tilt>
@@ -56,4 +57,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { BallCanvas } from './canvas'
+import SectionWrapper from '../hoc'
+import { technologies } from '../constants'
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology)=> (
+        <div className='w-28 h-32 flex flex-col items-center' key={technology.name}>
+          <BallCanvas icon={technology.icon}/>
+          <p className='text-white mt-2'>{technology.name}</p>
+        </div>
+      ))}
+
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "tech");
